934bf1de93a5f47fbe65f1b1c86429f79d0b5b3a
Qiaos1 <qiaos@middleton.school.nz>
2023-03-28 10:00:52 +1300

    Update

--- 01_Yes_No_V3.py
+++ 01_Yes_No_V3.py
@@ -1,21 +1,24 @@
 """LU Yes / No
-Simplifies the input by converting it to lower case. ALso, accepts y or n
-as abbreviations. Prints result of user choice as well as input - for testing
+Puts the code created in v2 into a loop to make testing easier and more
+efficient.
 """
 
-# Ask the user if they have played before
-show_instructions = input("Have you played this game before?: ").lower()
+show_instructions = ""
+while show_instructions != "x":
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before?: ").lower()
 
-# If they say yes,output 'Program Continues'
-if show_instructions == "yes" or show_instructions == "y":
-    print("Program continues")
+    # If they say yes,output 'Program Continues'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("Program continues")
 
-# If they say no, output 'Display Instructions'
-elif show_instructions == "no" or show_instructions == "n":
-    print("Display instructions")
+    # If they say no, output 'Display Instructions'
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
 
-# Otherwise - show error
-else:
-    print("Please answer Yes or No")
+    # Otherwise - show error
+    else:
+        print("Please answer Yes or No")
 
-print(f"You entered '{show_instructions}'")
+    print(f"You entered '{show_instructions}'")
+
